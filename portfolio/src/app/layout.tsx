b9704df13644201315aace0114a350835c2fb04f
jrza <devcontainers@Jazis-Lenovo>
2024-12-05 21:36:43 -0600

    trying favicon fix

--- portfolio/src/app/layout.tsx
+++ portfolio/src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon1.ico" />
         <meta name="theme-color" content="#ffffff" />
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
